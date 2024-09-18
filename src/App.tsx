02197506faf0a03d323dc0840995ccf0ec75d700
phuongn1810 <phuonglinhn9@gmail.com>
2024-09-18 16:40:07 -0400

    coming a2 branch

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Welcome to Webdev
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
